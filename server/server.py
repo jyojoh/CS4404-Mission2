dbad42225a0c9d2720fd7afe1ea6a62707f2b677
hzgand <101759753+hzgand@users.noreply.github.com>
2022-11-22 20:07:51 -0500

    Server login and 2fa

--- server/server.py
+++ server/server.py
@@ -2,6 +2,8 @@ from flask import Flask, session, render_template, request, redirect
 from flask_session import Session
 import database as init
 import library as db
+import random
+import string
 
 init.initDatabase()
 
@@ -10,21 +12,35 @@ app.secret_key = "twofaserver"
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-@app.route("/", methods=["GET", "POST"])
-def index():
+twoFactorCode = None
 
-    if request.method == "POST":
-        username = str(request.values.get("user"))
-        password = str(request.values.get("pass"))
+@app.get("/")
+def index_get():
+    return render_template("login.html")
 
-        if not db.checkUser(username, password):
-            return render_template("loginfail.html")
+@app.post("/login")
+def index_post():
+    username = str(request.values.get("user"))
+    password = str(request.values.get("pass"))
 
-        return render_template("twofactor.html")
+    if not db.checkUser(username, password):
+        return render_template("loginfail.html")
 
+    #Generate 2fa code, send to phone, and prompt for code
+    twoFactorCode = "".join(random.choices(string.digits, k=6))
+    ### Send twoFactorCode to phone here ###
+    print("Generated 2FA code: " + twoFactorCode + " for user " + username)
+    return render_template("twofactor.html")
 
-    return render_template("login.html", **data)
-
+@app.post("/twofactor")
+def twofactor_post():
+    #Check if code matches generated 2fa code
+    code = str(request.values.get("code"))
+    if code == twoFactorCode:
+        return render_template("success.html")
+    else:
+        print("Recieved code " + code + " expected code " + twoFactorCode)
+        return render_template("twofactorfail.html")
 
 if __name__ == '__main__':
     app.run("0.0.0.0", 80)
